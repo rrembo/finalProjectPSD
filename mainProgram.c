001a1ce12235823b6e08b2d964d48b37bf3f4a2c
LuqmanAristio <96041357+LuqmanAristio@users.noreply.github.com>
2021-12-15 17:08:56 +0700

    tambah fungsi

--- mainProgram.c
+++ mainProgram.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int session=-1;
+
+struct node{
+	int nim;
+	char nama[100];
+	char kelamin[10];
+	char tgl_lahir[100];
+	char tempat_lahir[100];
+	char alamat[100];
+	char kelas;
+	float UAS, UTS, quiz, tgs, rata;
+	char grade;
+
+	struct node *next;
+	struct node *prev;
+};
+
+struct node *head=NULL;
+
+void login();
+void DataMahasiswa();
+void NilaiMahasiswa();
+void Kelulusan();
+void showMahasiswa();
+void addMahasiswa();
+void deleteMahasiswa();
+void editMahasiswa();
+void header();
+void inputNilai();
+void dataNilai();
+void avgAll();
+void editNilai();
+void hapusNilai();
+float maxRataRata();
+float minRataRata();
+void avgGrade();
+void infoGrade();
+void lulus();
+void tidakLulus();
+void dataMhs(int);
+void editDataDiri(int);
+void cekLulus(int);
+
 void main(){
 
 
